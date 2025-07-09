7fffeb8fbf7219fbac356493b839e0d8b5251ab2
Medoed <stark49@mail.ru>
2025-07-09 19:30:30 +0300

    add navigation and fix views

--- management_system/urls.py
+++ management_system/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from .views import main_page, AppLoginView, AppLogoutView, RegisterView
+from .views import main_page, AppLoginView, AppLogoutView, RegisterView, MeetingView, TaskView, TeamView, EvaluationView
 
-app_name = "management_sys"
+app_name = "management_system"
 
 urlpatterns = [
     path('', main_page, name='main_page'),
     path('login/', AppLoginView.as_view(), name='login'),
     path('logout/', AppLogoutView.as_view(), name='logout'),
-    path('registration/', RegisterView.as_view(), name='registration')
+    path('registration/', RegisterView.as_view(), name='registration'),
+    path('meeting/', MeetingView.as_view(), name='meeting'),
+    path('task/', TaskView.as_view(), name='task'),
+    path('team/', TeamView.as_view(), name='team'),
+    path('evaluation/', EvaluationView.as_view(), name='evaluation'),
+
 ]
--- management_system/views.py
+++ management_system/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Avg, Func, F
+from django.db.models.functions import ExtractMonth, TruncMonth
 from django.views import View
 from django.views.generic import FormView
 from django.shortcuts import render, redirect
@@ -6,27 +8,33 @@ from django.contrib.auth.models import AnonymousUser
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from datetime import datetime, date, timedelta
 
-from .models import User
+from .models import User, Meeting, Task, Team, UserTeam, Evaluation
 from .forms import RegisterForm
+
+
+class DateToChar(Func):
+    function = 'TO_CHAR'
+    template = "%(function)s(%(expressions)s, 'YYYY-MM')"
 
 
 class AppLoginView(LoginView):
     template_name = 'login.html'
     redirect_authenticated_user = True
-    next_page = reverse_lazy("management_sys:main_page")
+    next_page = reverse_lazy("management_system:main_page")
 
 
 class AppLogoutView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         logout(request)
-        return redirect("management_sys:login")
+        return redirect("management_system:login")
 
 
 class RegisterView(FormView):
     form_class = RegisterForm
     template_name = 'registration/registration.html'
-    success_url = reverse_lazy("management_sys:main_page")
+    success_url = reverse_lazy("management_system:main_page")
 
     def form_valid(self, form):
         user = form.save()
@@ -36,6 +44,53 @@ class RegisterView(FormView):
 
 def main_page(request):
     if not hasattr(request, "user") or isinstance(request.user, AnonymousUser):
-        return redirect("management_sys:login")
+        return redirect("management_system:login")
 
     return render(request, 'index.html')
+
+
+class MeetingView(View):
+    def get(self, request, *args, **kwargs):
+        if not hasattr(request, "user") or isinstance(request.user, AnonymousUser):
+            return redirect("management_system:login")
+        user_id = request.user.id
+        user_meetings = Meeting.objects.filter(meeting_members__user=request.user)
+        context = {"meetings": user_meetings}
+        return render(request, 'meetings.html', context=context)
+
+
+class TaskView(View):
+    def get(self, request, *args, **kwargs):
+        if not hasattr(request, "user") or isinstance(request.user, AnonymousUser):
+            return redirect("management_system:login")
+        user_id = request.user.id
+        user_tasks = Task.objects.filter(executor=request.user)
+        context = {"tasks": user_tasks}
+        return render(request, 'tasks.html', context=context)
+
+
+class TeamView(View):
+    def get(self, request, *args, **kwargs):
+        if not hasattr(request, "user") or isinstance(request.user, AnonymousUser):
+            return redirect("management_system:login")
+        user_teams = UserTeam.objects.filter(user=request.user)
+        context = {"user_teams": user_teams}
+        return render(request, 'teams.html', context=context)
+
+
+class EvaluationView(View):
+    def get(self, request, *args, **kwargs):
+        if not hasattr(request, "user") or isinstance(request.user, AnonymousUser):
+            return redirect("management_system:login")
+        user_evaluation = Evaluation.objects.filter(user=request.user)
+        context = {"user_evaluations": user_evaluation}
+        today = date.today()
+        year_start = date(today.year, 1, 1)
+        data = (Evaluation.objects
+                .filter(updated_at__range=(year_start, today))
+                .annotate(month=DateToChar(TruncMonth('updated_at')))
+                .values('month')
+                .annotate(avg_mark=Avg('mark')))
+        context["average_marks"] = data
+
+        return render(request, 'evaluation.html', context=context)
